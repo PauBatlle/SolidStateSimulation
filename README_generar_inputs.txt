616fc875e85e5e5ec59bbee808ad80d87cf6b562
PauBatlle <paubatlle11@outlook.com>
2018-02-19 08:56:45 +0100

    Input Generator v1

--- README_generar_inputs.txt
+++ README_generar_inputs.txt
@@ -1,11 +1,13 @@
 Format dels inputs:
+
 nom del model
 n <-- Nombre de partícules
 m1 m2 ... mn <-- Masses de les n particules
 x1 y1 z1 <-- Posicio inicial particula 1
-vx1 vy1 vz1 <-- Velocitat inicial particula 1
-...
+[...]
 xn yn zn <-- Posicio inicial particula n 
+vx1 vy1 vz1 <-- Velocitat inicial particula 1
+[...]
 vxn vyn vzn <-- Velocitat inicial particula n
 
 *** EXEMPLE *** 
@@ -17,3 +19,5 @@ Prova
 0 0 1
 0 0 0
 ******
+
+Els inputs es poden generar aleatòriament usant l'script input_generator.py
---
+++ input_generator.py
@@ -0,0 +1,71 @@
+import argparse
+import logging
+import numpy as np
+import random
+from random import uniform as rnd
+## Codi per generar inputs llegibles, a partir d'uns certs paràmetres d'entrada
+
+## Per reproducibilitat
+random.seed(23)
+
+## Part per modificar
+
+npart = 10 #Nombre de partícules
+xmax = 3 #Les x inicials estaran uniformes entre [xmin, xmax]
+xmin = -xmax
+ymax = 3 #Les y inicials estaran uniformes entre [ymin, ymax]
+ymin = -ymax
+zmax = 3 #Les z inicials estaran uniformes entre [zmin, zmax]
+zmin = -zmax
+vxmax = 3 #Les vx inicials estaran uniformes entre [vxmin, vxmax]
+vxmin = -vxmax
+vymax = 3 #Les vy inicials estaran uniformes entre [vymin, vtmax]
+vymin = -vymax
+vzmax = 3 #Les vz inicials estaran uniformes entre [vzmin, vzmax]
+vzmin = -vzmax
+massmax = 1 #Les masses estaran uniformes entre [massmin, massmax]
+massmin = 0
+
+nom_model = "Exemple"
+output = "InputsGenerats/Exemple_output_generat_per_input_generator"
+
+## No tocar a partir d'aquí
+
+aux_vec = [xmax, ymax, zmax]
+aux_vec_m = [xmin, ymin, zmin]
+
+aux_vec_v = [vxmax, vymax, vzmax]
+aux_vec_vm = [vxmin, vymin, vzmin]
+
+posicions = np.zeros((npart, 3))
+velocitats = np.zeros((npart, 3))
+
+for i in range(3):
+	posicions[:,i] = np.array([rnd(aux_vec_m[i], aux_vec[i]) for j in range(npart)])
+	velocitats[:,i] = np.array([rnd(aux_vec_vm[i], aux_vec_v[i]) for j in range(npart)])
+
+masses = [rnd(massmin, massmax) for i in range(npart)]
+
+#Llegeix en un fitxer
+with open(output, "w") as fitxer:
+	fitxer.write(nom_model + '\n')
+	fitxer.write(str(npart) + '\n')
+	for i in range(npart):
+		if (i):
+			fitxer.write(" ")
+		fitxer.write(str(masses[i])) 
+	fitxer.write('\n')
+
+	for i in range(npart):
+		for j in range(3):
+			if (j):
+				fitxer.write(" ")
+			fitxer.write(str(posicions[i, j]))
+		fitxer.write('\n')
+
+	for i in range(npart):
+		for j in range(3):
+			if (j):
+				fitxer.write(" ")
+			fitxer.write(str(velocitats[i, j]))
+		fitxer.write('\n')
