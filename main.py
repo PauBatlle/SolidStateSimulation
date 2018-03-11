3bda966ae4fa1f8161bd3c7e841c8bba55dd9e64
PauBatlle <paubatlle11@outlook.com>
2018-03-11 19:04:19 +0100

    Una mica d'estructura pel saver

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
 import argparse
+import os
 from tqdm import tqdm as t 
 import numpy as np
 from input_reader import llegeix
 import integradors
 from acc import acceleracions
 from IPython import embed
+
+from sys import exit as rop
 
 class parameters():
 	def __init__(self, niter, timestep, sigma, eps, integrador):
@@ -22,12 +25,39 @@ class parameters():
 		self.B = 4*self.eps*np.power(self.sigma, 12)
 		self.r_m = self.sigma*np.power(2, 1/6) #Distància amb el mínim de potencial
 
+	def __str__(self):
+		s = ""
+		s = s + "-Sig:" + str(round(self.sigma, 4))
+		s = s + "-Eps:" + str(round(self.eps, 4))
+		s = s + "-Niter:" + str(self.niter)
+		s = s + "-logtstep:" + str(round(np.log10(self.timestep), 4))
+		s = s + "-Integr:" + self.integrador
+		return s
+
 class Experiment():
 	def __init__(self, fitxer_entrada, params):
 		self.initial = llegeix(fitxer_entrada)
 		self.params = params
-		self.itera()
-		self.postprocessa()
+		self.unique_str = self.initial["model"]+str(self.params)
+		if not self.done_before():
+			self.itera()
+			self.postprocessa()
+			self.save()
+		#os.system("rm "+"-r "+self.directory) #S'ha de borrar això després, és temporal!!
+	def done_before(self):
+		"""
+		Miro si ja he fet abans aquest experiment.
+		"""
+		logging.info("Comprovant si ja s'ha calculat la simulació")
+		self.directory = "Resultats/"+self.unique_str
+		if not os.path.exists(self.directory):
+			os.makedirs(self.directory)
+			logging.info("No s'ha calculat, creant directori per guardar els resultats")
+			return False
+		logging.info("Simulació ja calculada, " + self.directory)
+		return True
+		
+
 
 	def itera(self):
 		npart = self.initial["npart"]
@@ -47,13 +77,23 @@ class Experiment():
 			acc_act = acceleracions(pos_act, masses, self.params.A, self.params.B)
 			self.accelerations[:,:,step] = acc_act
 			pos_act, vel_act = integrador(pos_act, vel_act, acc_act, timestep)
-	
+		logging.info("")
 	def postprocessa(self):
 		logging.info("Simulació acabada correctament, processant resultats")		
 		#Energies potencial i cinètica
+		
 		#Trajectòries
+		
+		#Video
 		#WIP
-		pass	
+		pass
+
+	def save(self):
+		logging.info("Guardant els resultats")
+		np.save(self.directory+"/positions", self.positions)
+		np.save(self.directory+"/velocities", self.velocities)
+		np.save(self.directory+"/accelerations", self.accelerations)
+		pass
 
 ### Paràmetres Default
 niter = 5000
